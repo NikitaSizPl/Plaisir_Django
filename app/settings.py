921a54ef5dd946f1a634b883e42c2930311b47ed
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-09 22:53:37 +0100

    add apps: 'user', 'orders'

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'user',
+    'orders'
+
 ]
 
 MIDDLEWARE = [
