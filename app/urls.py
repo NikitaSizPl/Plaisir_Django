6710325f0c134586b8d41c8123cbb980f0adcf3b
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-09 22:55:08 +0100

    add urls: path('', include('user.urls')) path('', include('orders.urls'))

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('products.urls')),
-    path('admin/', admin.site.urls),
 
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('', include('user.urls')),
+    path('', include('orders.urls')),
 ]
