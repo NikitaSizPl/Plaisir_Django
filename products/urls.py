9fb2eb30f83f9aa67793722d70c0442e6643583e
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-16 06:17:43 +0100

    from .views import *
        path("", index, name="index"),
        path("delivery/", delivery, name="delivery"),
        path("infopay/", infopay, name="infopay"),
        path("contact/", contact, name="contact"),
        path("categories/", categories, name="categories"),
        path("categories/<str:cat_name>/items", items_categor, name="items_categor"),
        path('item/<str:item_str>', item_id, name='item_id')

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import categories, items_categor, item_id
+from .views import *
 
 urlpatterns = [
 
-    path("", categories, name="categories"),
+    path("", index, name="index"),
+    path("delivery/", delivery, name="delivery"),
+    path("infopay/", infopay, name="infopay"),
+    path("contact/", contact, name="contact"),
+    path("categories/", categories, name="categories"),
     path("categories/<str:cat_name>/items", items_categor, name="items_categor"),
     path('item/<str:item_str>', item_id, name='item_id')
 ]
