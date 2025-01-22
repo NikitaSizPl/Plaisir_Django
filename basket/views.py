dfc5f0173cf14eebae62d0a8d4471c8055afb75f
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-22 06:36:47 +0100

    add base basket: def get_or_creat_basket(request):     basket_id = request.session.get('basket_id')     if not basket_id:         basket = Basket.objects.create()         request.session['basket_id'] = basket.id     else:         basket = get_object_or_404(Basket, id=basket_id)     return basket

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Basket, BasketItem
+from products.models import Item
+
 
 
 # Create your views here.
+def get_or_creat_basket(request):
+    basket_id = request.session.get('basket_id')
+    if not basket_id:
+        basket = Basket.objects.create()
+        request.session['basket_id'] = basket.id
+    else:
+        basket = get_object_or_404(Basket, id=basket_id)
+    return basket
+
+
+
 def basket(request):
-    return render(request,
-                  "basket/get_all.html"
-                   )
+   basket = get_or_creat_basket(request)
+   context = {'basket': basket}
+   return render(request,
+                 'basket/get_all.html',
+                 context)
+
+def add_to_basket(request, product_id):
+    item = get_object_or_404(Item, id=product_id)
+    basket = get_or_creat_basket(request)
+    quantity = int(request.POST.get('quantity', 1))
+    basket_item, creat = BasketItem.objects.get_or_create(basket=basket, item=item)
+    if creat:
+        basket_item.quantity = quantity
+    else:
+        basket_item.quantity += quantity
+    basket_item.save()
+    return redirect("basket")
+
+def remove_to_basket(request, product_id):
+    item = get_object_or_404(Item, id=product_id)
+    basket = get_or_creat_basket(request)
+    basket_item = BasketItem.objects.get_or_create(basket=basket, item=item)
+    if basket_item:
+        basket_item.quantity -= 1
+        if basket_item <= 0:
+            basket_item.delete()
+        else:
+            basket_item.save()
+    return redirect("basket")
