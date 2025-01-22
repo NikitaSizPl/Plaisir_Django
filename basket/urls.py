9e651de123d298e8153ff085cd76a3e8bc42691e
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-22 06:37:12 +0100

    add:
    urlpatterns = [
    
        path("", basket, name="basket"),
        path('add/<int:product_id>/', add_to_basket, name='add_to_basket'),
        path('remove/<int:product_id>/', remove_to_basket, name='add_to_basket'),
    ]

--- basket/urls.py
+++ basket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import basket
+from .views import basket, add_to_basket, remove_to_basket
 
 urlpatterns = [
 
-    path("", basket, name="basket")
-
+    path("", basket, name="basket"),
+    path('add/<int:product_id>/', add_to_basket, name='add_to_basket'),
+    path('remove/<int:product_id>/', remove_to_basket, name='add_to_basket'),
 ]
