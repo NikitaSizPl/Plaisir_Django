d74637234bf1236eb91ab203447cf87f985fd0a2
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-22 06:35:39 +0100

    add: def get_users(request):     users = User.objects.all()     context = {         "users": users     }     return render(request, 'user/profile.html', context)

--- user/views.py
+++ user/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import User
 
 # Create your views here.
 
-def profil(request):
-    return render(request,
-                  'user/profile.html'
+def get_users(request):
+    users = User.objects.all()
+    context = {
+        "users": users
+    }
+    return render(request, 'user/profile.html', context)
 
-                  )
