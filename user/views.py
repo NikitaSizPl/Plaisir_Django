fd0aa03a6afe66d350a75666fe6d4e4104dff024
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-25 21:09:55 +0100

    def login_user(request):     if request.method == 'POST':         username = request.POST['username']         password = request.POST['password']         user = authenticate(request, name=username, password=password)         if user is not None:             login(request, user)             return redirect('index')         else:             messages.error(request, "Неверное имя пользователя или пароль")             return profile(request, username)     else:         return render(request, 'user/login.html')
    def creat_user(request):
        if request.method == "POST":
            username = request.POST['username']
            password = request.POST['password']
            if User.objects.filter(name=username).exists():
                messages.error(request, f'{username} - уже зарегистрирован')
                return redirect('creat')
            user = User.objects.create(name=username, password=password)
            user.save()
            return redirect('index')
        return render(request, 'user/creat.html')

--- user/views.py
+++ user/views.py
@@ -10,3 +10,31 @@ def get_users(request):
     }
     return render(request, 'user/profile.html', context)
 
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, name=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль")
+            return profile(request, username)
+    else:
+        return render(request, 'user/login.html')
+
+def creat_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        if User.objects.filter(name=username).exists():
+            messages.error(request, f'{username} - уже зарегистрирован')
+            return redirect('creat')
+        user = User.objects.create(name=username, password=password)
+        user.save()
+        return redirect('index')
+    return render(request, 'user/creat.html')
+
+
