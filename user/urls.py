0bed7377bf08d9247add3a3fed7a47e034536903
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-25 22:29:08 +0100

    urlpatterns = [     path('login', login_user, name="login"),     path('creat', creat_user, name='creat'),     path('user/<str:username>', profile, name='profile')
    ]

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,9 @@ from .views import profil
 
 
 urlpatterns = [
+    path('login', login_user, name="login"),
+    path('creat', creat_user, name='creat'),
+    path('user/<str:username>', profile, name='profile')
 
     path('profile', profil, name="profil")
 ]
