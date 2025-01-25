337e914d128925c3636d90606aa7048f3c2dfbd9
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-01-25 22:29:48 +0100

    urlpatterns = [
        path("", index, name="index"),
        path("categories/", categories, name="categories"),
        path("categories/<str:cat_name>/items", items_categor, name="items_categor"),
        path('item/<int:item_id>', item_id, name='item_id')
    ]

--- products/urls.py
+++ products/urls.py
@@ -5,10 +5,7 @@ from .views import *
 urlpatterns = [
 
     path("", index, name="index"),
-    path("delivery/", delivery, name="delivery"),
-    path("infopay/", infopay, name="infopay"),
-    path("contact/", contact, name="contact"),
     path("categories/", categories, name="categories"),
     path("categories/<str:cat_name>/items", items_categor, name="items_categor"),
-    path('item/<str:item_str>', item_id, name='item_id')
+    path('item/<int:item_id>', item_id, name='item_id')
 ]
