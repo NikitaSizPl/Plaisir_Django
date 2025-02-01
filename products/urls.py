561ff8d9fff5ca0d771bd0dd01cd05f5da70f98a
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-01 14:17:13 +0100

    refresh urlpatterns add: app_name = 'products'

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from .views import *
+
+app_name = 'products'
 
 urlpatterns = [
 
     path("", index, name="index"),
     path("categories/", categories, name="categories"),
-    path("categories/<str:cat_name>/items", items_categor, name="items_categor"),
-    path('item/<int:item_id>', item_id, name='item_id')
+    path("categories/<str:cat_name>", items_categor, name="items_categor"),
+    path('item/<int:product_id>', product_id, name='item_id'),
 ]
