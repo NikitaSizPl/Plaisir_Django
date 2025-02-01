2929fdb2c9c1e0e9cde828a12d87110e4579dcf8
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-01 14:43:11 +0100

    add: def del_all

--- basket/views.py
+++ basket/views.py
@@ -1,48 +1,51 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Basket, BasketItem
-from products.models import Item
-
+from django.shortcuts import render, redirect, get_object_or_404
+from basket.models import Basket, BasketItem
+from products.models import Product
 
 
 # Create your views here.
-def get_or_creat_basket(request):
-    basket_id = request.session.get('basket_id')
-    if not basket_id:
-        basket = Basket.objects.create()
-        request.session['basket_id'] = basket.id
+def basket_base(request):
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user).first()
+        if not basket:
+            basket = Basket.objects.create(user=request.user)
     else:
-        basket = get_object_or_404(Basket, id=basket_id)
-    return basket
+        basket_id = request.session.get('basket_id')
+        if not basket_id:
+            basket = Basket.objects.create()
+            request.session['basket_id'] = basket.id
+        else:
+            basket = Basket.objects.get(id=basket_id)
+    context = {
+        'basket': basket,
+        'basket_item': BasketItem.objects.filter(basket=basket)
+    }
+    return render(request, 'basket/basket.html', context)
 
-
-
-def basket(request):
-   basket = get_or_creat_basket(request)
-   context = {'basket': basket}
-   return render(request,
-                 'basket/get_all.html',
-                 context)
 
 def add_to_basket(request, product_id):
-    item = get_object_or_404(Item, id=product_id)
-    basket = get_or_creat_basket(request)
-    quantity = int(request.POST.get('quantity', 1))
-    basket_item, creat = BasketItem.objects.get_or_create(basket=basket, item=item)
-    if creat:
-        basket_item.quantity = quantity
-    else:
-        basket_item.quantity += quantity
-    basket_item.save()
-    return redirect("basket")
-
-def remove_to_basket(request, product_id):
-    item = get_object_or_404(Item, id=product_id)
-    basket = get_or_creat_basket(request)
-    basket_item = BasketItem.objects.get_or_create(basket=basket, item=item)
-    if basket_item:
-        basket_item.quantity -= 1
-        if basket_item <= 0:
-            basket_item.delete()
-        else:
+    product = get_object_or_404(Product, id=product_id)
+    basket, create = Basket.objects.get_or_create(user=request.user)
+    basket_item, create = BasketItem.objects.get_or_create(basket=basket, product=product, quantity=1)
+    if not create:
+        if product in basket_item:
+            basket_item.quantity += 1
             basket_item.save()
-    return redirect("basket")
+    return redirect('basket:basket')
+
+
+def del_from_basket(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    basket, create = Basket.objects.get_or_create(user=request.user)
+    basket_item, create = BasketItem.objects.get_or_create(basket=basket, product=product)
+    if not create:
+        basket_item.quantity -= 1
+        basket_item.save()
+    return redirect('basket:basket')
+
+
+def del_all(request):
+    basket = Basket.objects.get(user=request.user)
+    if basket:
+        basket.clear()
+    return redirect('basket:basket')
