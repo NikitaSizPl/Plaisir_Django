3b81282f6a275a05259316d7b0d91c46acdbca17
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-01 14:38:32 +0100

    refresh Item -> Product

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Catigories, Item
+from .models import Catigories, Product
 
 # Register your models here.
 admin.site.register(Catigories)
-admin.site.register(Item)
+admin.site.register(Product)
