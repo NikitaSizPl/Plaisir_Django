7984914bfc000601d23a34d07ee71e81a8222300
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-01 14:36:56 +0100

    add: urlpatterns 'del/all'

--- basket/urls.py
+++ basket/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import basket_base, add_to_basket, del_from_basket
+from .views import basket_base, add_to_basket, del_from_basket, del_all
 
 app_name = 'basket'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('basket/', basket_base, name='basket'),
     path('add/<int:product_id>/', add_to_basket, name='add_to_basket'),
     path('del/<int:product_id>/', del_from_basket, name='del_from_basket'),
+    path('del/all/', del_all, name='del_all'),
     # path('upg/<int:product_id>/', upg_from_basket, name='upg_from_basket'),
 ]
