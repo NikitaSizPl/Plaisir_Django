83e24047d42614289d695fd591b95eaee12356aa
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-12 20:44:52 +0100

    None

--- products/models.py
+++ products/models.py
@@ -3,17 +3,21 @@ from django.db import models
 
 # Модель категории
 class Catigories(models.Model):
-    name = models.CharField(max_length=100,
-                            verbose_name='Название'
-                            )
-    description = models.CharField(max_length=255,
-                                   blank=True,
-                                   verbose_name='Описание',
-                                   )
-    image_id = models.IntegerField(null=True,
-                                   blank=True,
-                                   verbose_name='Изображение'
-                                   )
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+        )
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Описание',
+        )
+    image = models.ImageField(
+        upload_to='images',
+        null=True,
+        blank=True,
+        verbose_name='Изображение'
+        )
 
     def __str__(self):
         return self.name
@@ -25,30 +29,36 @@ class Catigories(models.Model):
 
 # Модель товара
 class Product(models.Model):
-    name = models.CharField(max_length=100,
-                            null=True,
-                            verbose_name='Название'
-                            )
-    description = models.CharField(max_length=255,
-                                   blank=True,
-                                   verbose_name='Описание',
-                                   )
-    price = models.FloatField(null=True,
-                              blank=True,
-                              verbose_name='Цена'
-                              )
-    image_id = models.IntegerField(null=True,
-                                   blank=True,
-                                   verbose_name='Изображение'
-                                   )
+    name = models.CharField(
+        max_length=100,
+        null=True,
+        verbose_name='Название'
+        )
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Описание',
+        )
+    price = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Цена'
+        )
+    image = models.ImageField(
+        upload_to='images',
+        null=True,
+        blank=True,
+        verbose_name='Изображение'
+        )
     # Зависимость к классу Catigories
-    categories = models.ForeignKey(Catigories,
-                                   on_delete=models.CASCADE,
-                                   verbose_name='Категория'
-                                   )
+    categories = models.ForeignKey(
+        Catigories,
+        on_delete=models.CASCADE,
+        verbose_name='Категория'
+        )
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'Товар'
