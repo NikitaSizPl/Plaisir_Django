51f3e7499aea7170637f31ab471ea870b399cf46
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-12 20:46:05 +0100

    creats: class CategoriesAdmin(admin.ModelAdmin), class ProductAdmin(admin.ModelAdmin)

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Catigories, Product
 
 # Register your models here.
-admin.site.register(Catigories)
-admin.site.register(Product)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'image']
+    list_editable = ['description', 'image']
+    prepopulated_fields = {'name': ('name',)}
+admin.site.register(Catigories, CategoriesAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'image']
+    list_editable = ['description', 'price', 'image']
+    prepopulated_fields = {'name': ('name',)}
+admin.site.register(Product, ProductAdmin)
