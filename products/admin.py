2651eb22357d4644336b2090080fc8caa52d109c
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-15 23:29:12 +0100

    add ordering

--- products/admin.py
+++ products/admin.py
@@ -6,10 +6,12 @@ class CategoriesAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'image']
     list_editable = ['description', 'image']
     prepopulated_fields = {'name': ('name',)}
+    ordering = ('id',)
 admin.site.register(Catigories, CategoriesAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'price', 'image']
     list_editable = ['description', 'price', 'image']
     prepopulated_fields = {'name': ('name',)}
+    ordering = ('id',)
 admin.site.register(Product, ProductAdmin)
