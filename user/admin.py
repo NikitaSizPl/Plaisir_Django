88b83872faf30eacf6498bde70ffb6bada6a4c10
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-15 23:41:49 +0100

    add inlines = (BasketAdmin,)

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import User
-
+from products.admin import BasketAdmin
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'instagram', 'phone')
-
-
-# Register your models here.
+    inlines = (BasketAdmin,)
 admin.site.register(User, UserAdmin)
