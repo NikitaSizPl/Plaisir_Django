fb509befdf8e95b1c0b1079494792d2919526c0f
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-02-18 10:03:28 +0100

    add class OrderItemInline(admin.TabularInline), class OrderAdmin(admin.ModelAdmin):

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import OrderItem, Order
 
-# Register your models here.
+# Register your models here
+
+
+class OrderItemInline(admin.TabularInline):  # Define inline for OrderItem
+    model = OrderItem
+    raw_id_fields = ['order', 'product']  # Optional: Use raw_id_fields to show a raw input for the ForeignKey (Product)
+
+class OrderAdmin(admin.ModelAdmin):  # Customize OrderAdmin
+    list_display = ['id', 'user']
+    inlines = [OrderItemInline]  # Include OrderItem inline in the OrderAdmin
+
+# Register models in admin site
+admin.site.register(Order, OrderAdmin)  # Register Order with the customized admin
 admin.site.register(OrderItem)
-admin.site.register(Order)
