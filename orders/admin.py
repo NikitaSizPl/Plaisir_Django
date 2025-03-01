31f477e7944978e799072f9f560a75119a6a8f0a
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-03-01 18:32:09 +0100

    create orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from .models import OrderItem, Order
+from .models import Order, OrderItem, OrderInfoForm
+# Register your models here.
 
-# Register your models here
+class OrderItemInline(admin.TabularInline):
+    model = OrderInfoForm
 
 
-class OrderItemInline(admin.TabularInline):  # Define inline for OrderItem
-    model = OrderItem
-    raw_id_fields = ['order', 'product']  # Optional: Use raw_id_fields to show a raw input for the ForeignKey (Product)
-
-class OrderAdmin(admin.ModelAdmin):  # Customize OrderAdmin
+class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'user']
-    inlines = [OrderItemInline]  # Include OrderItem inline in the OrderAdmin
+    inlines = [OrderItemInline]
 
-# Register models in admin site
-admin.site.register(Order, OrderAdmin)  # Register Order with the customized admin
-admin.site.register(OrderItem)
+
+admin.site.register(Order, OrderAdmin)
+
