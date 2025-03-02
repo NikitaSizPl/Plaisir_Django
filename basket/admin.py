2d883b98a1d646fb50f6f0346d8a913f232e565a
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-03-02 18:19:19 +0100

    create class BasketItemInline, class BasketAdmin

--- basket/admin.py
+++ basket/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Basket, BasketItem
 
+class BasketItemInline(admin.TabularInline):
+    model = BasketItem
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price']
+    inlines = [BasketItemInline]
+
 # Register your models here.
-admin.site.register(Basket)
-admin.site.register(BasketItem)
+admin.site.register(Basket, BasketAdmin)
